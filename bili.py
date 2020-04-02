f15ef559e1c70b7e8e283e3bdcdba127087a4c9b
Raven <45040536+hoijunx@users.noreply.github.com>
2020-04-03 00:57:41 +0800

    Update bili.py

--- bili.py
+++ bili.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 
-url = 'https://oapi.dingtalk.com/robot/send?access_token=dfb282779fa574887abaade18616db9be415fcc4b11289a6bd487a59f2b5a641'
+url = 'https://oapi.dingtalk.com/robot/send?access_token=80e4251b904f3088f7959d1de12191d9cb7c9a83d8061fa936d3e42e03188106'
 # 半佛，朱一旦，大骚,罗翔说刑法,毕导THU,何同学
 bili_ids = ['37663924','437316738','390461123','517327498','254463269','163637592']
 yesterday = time.time()-60*60*24*2  # 1天前
