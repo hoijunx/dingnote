f7d53262fe49f4e3d3aa3f1ec9662ee2a9004b13
Raven <45040536+hoijunx@users.noreply.github.com>
2020-04-04 19:30:07 +0800

    Update bili.py

--- bili.py
+++ bili.py
@@ -1,1 +1,18 @@
-print （“今天周一，记得交学习强国稿件报送表哦！！”）
+import json
+import requests
+ 
+def dingding_409robot(data):
+    webhook = "https://oapi.dingtalk.com/robot/send?access_token=80e4251b904f3088f7959d1de12191d9cb7c9a83d8061fa936d3e42e03188106"
+    headers = {'content-type': 'application/json'} # 请求头
+    r = requests.post(webhook, headers=headers, data=json.dumps(data))
+    r.encoding = 'utf-8'
+    return (r.text)
+ 
+ 
+if __name__ == "__main__":
+    data = {
+        "msgtype": "text",
+        "text": { "content":"409机器人提醒你：今天周一，记得发送" }
+        }
+    res = dingding_409robot(data)
+    print(res) 
