d03f616b5558083ee3146597cda03ca32139289c
Raven <45040536+hoijunx@users.noreply.github.com>
2020-04-04 20:01:37 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -1,1 +1,19 @@
-url = 'https://oapi.dingtalk.com/robot/send?access_token=dfb282779fa574887abaade18616db9be415fcc4b11289a6bd487a59f2b5a641'
+import json
+import requests
+ 
+def dingding_409robot(data):
+    webhook = "https://oapi.dingtalk.com/robot/send?access_token=80e4251b904f3088f7959d1de12191d9cb7c9a83d8061fa936d3e42e03188106"
+    headers = {'content-type': 'application/json'} # 请求头
+    r = requests.post(webhook, headers=headers, data=json.dumps(data))
+    r.encoding = 'utf-8'
+    return (r.text)
+ 
+ 
+if __name__ == "__main__":
+    data = {
+        "msgtype": "text",
+        "text": { "content":"我是机器人2号，提醒你测试成功" }
+        }
+    res = dingding_409robot(data)
+    print(res) 
+
